886ad74d99d7224e52ab160fb17e19c54e55b4df
Aaron Meyer <asmeyer.physics@gmail.com>
2022-01-03 17:11:04 -0800

    update lqcd summary plot range and colors

--- py/gaq2_summary/make_plot.py
+++ py/gaq2_summary/make_plot.py
@@ -64,7 +64,7 @@ rqcd = get_rqcd( 'rqcd_gaq2_zexp4+3.dat')
 
 FA_Q2_fn, _  = define_FA_zexp_sum4( 4, **params_1603_03048)
 
-Q2_range = np.arange( 0.0, 0.62, 0.01)
+Q2_range = np.arange( 0.0, 1.02, 0.01)
 FA_Q2 = np.array([ FA_Q2_fn( Q2, ak_1603_03048) for Q2 in Q2_range ])
 
 ymean = gv.mean( FA_Q2)
@@ -80,7 +80,7 @@ plt.rcParams[ 'errorbar.capsize'] = 5
 f0 = plt.figure()
 f0.set_size_inches( *figure_size_default)
 plt.subplots_adjust( *plot_axes_default)
-plt.fill_between( Q2_range, ym, yp, color='red', alpha=0.3)
+plt.fill_between( Q2_range, ym, yp, color='red', alpha=0.3, zorder=-5)
 
 lbc = 'CLS 17'
 lbe = 'ETMC 20'
@@ -90,27 +90,29 @@ lbp = 'PACS 18 erratum'
 lbp21= 'PACS 21'
 lbn = 'NME 21'
 lbr = 'RQCD 20'
-phc = plt.errorbar( cls.T[0], cls.T[1], yerr=cls.T[2],
-  color=fuschia, marker='^', linestyle='', label=lbc)
-phe = plt.errorbar( etm.T[0], etm.T[1], yerr=etm.T[2],
-  color=orange,  marker='o', linestyle='', label=lbe)
-phm = plt.errorbar( mainz.T[0], mainz.T[1], yerr=mainz.T[2],
-  color=grey,    marker='v', linestyle='', label=lbm)
-phx = plt.errorbar( callat.T[0], callat.T[1], yerr=callat.T[2],
-  color=sunkist, marker='*', linestyle='', label=lbx)
-php = plt.errorbar( pacs.T[0], pacs.T[1], yerr=pacs.T[2],
-  color=violet,  marker='s', linestyle='', label=lbp)
-php21 = plt.errorbar( pacs21.T[0], pacs21.T[1], yerr=pacs21.T[2],
-  color=violet,  marker='s', markerfacecolor='none', linestyle='', label=lbp21)
-phn = plt.fill_between( nme.T[0],  nme.T[1]  -nme.T[2],  nme.T[1]  +nme.T[2],
-  color=blue,  alpha=0.3, label=lbn)
+## use zorder to change plot order for errorbars
 phr = plt.fill_between( rqcd.T[0], rqcd.T[1] -rqcd.T[2], rqcd.T[1] +rqcd.T[2],
-  color=green, alpha=0.3, label=lbr)
+  color=green, alpha=0.3, label=lbr, zorder=-3)
+php = plt.errorbar( pacs.T[0], pacs.T[1], yerr=pacs.T[2],
+  color='#508EED',   marker='s', linestyle='', label=lbp, zorder=-2)
+php21 = plt.errorbar( pacs21.T[0], pacs21.T[1], yerr=pacs21.T[2],
+  color='#508EED',   marker='s', markerfacecolor='none', linestyle='', label=lbp21, zorder=-2)
+phc = plt.errorbar( cls.T[0], cls.T[1], yerr=cls.T[2],
+  color=sunkist,  marker='^', linestyle='', label=lbc, zorder=-1)
+phe = plt.errorbar( etm.T[0], etm.T[1], yerr=etm.T[2],
+  color='#3CB24C',   marker='o', linestyle='', label=lbe, zorder=0)
+phm = plt.errorbar( mainz.T[0], mainz.T[1], yerr=mainz.T[2],
+  color='#606060',     marker='v', linestyle='', label=lbm, zorder=1)
+phx = plt.errorbar( callat.T[0], callat.T[1], yerr=callat.T[2],
+  color='#FF0F0F',marker='*', linestyle='', label=lbx, zorder=2)
+phn = plt.fill_between( nme.T[0],  nme.T[1]  -nme.T[2],  nme.T[1]  +nme.T[2],
+  color=violet,  alpha=0.4, label=lbn, zorder=3)
 
-plt.xlim([-0.009,0.6])
-plt.ylim([ 0.6, 1.35])
+plt.xlim([-0.009,1.0])
+plt.ylim([ 0.3, 1.35])
 #plt.axis('off')
-plt.text(x=0.25, y=0.65, s=r'$\nu$D $z$ exp', color='red', rotation=-30, fontsize=20)
+#plt.text(x=0.25, y=0.65, s=r'$\nu$D $z$ exp', color='red', rotation=-30, fontsize=20) ## old bds
+plt.text(x=0.25, y=0.60, s=r'$\nu$D $z$ exp', color='red', rotation=-30, fontsize=14)
 
 plt.legend(
   [phn, phr, phm, phx, php21, php, phe, phc],
